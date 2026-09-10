5eb3b2014b8e94152884e9134960c5827f0a6aca
kishore-3101 <purukiss@gmail.com>
2026-09-10 16:06:46 +0530

    add: add bluetooth toggle script

---
+++ sway/scripts/bluetooth_toggle.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if bluetoothctl show | grep -q "Powered: yes"; then
+    bluetoothctl power off
+    notify-send "Bluetooth Disabled"
+else
+    bluetoothctl power on
+    notify-send "Bluetooth Enabled"
+fi
